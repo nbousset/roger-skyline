0883ced1250c858c5d0f3d5a5a512c36908e555e
nbousset <nbousset@student.42.fr>
2020-12-23 16:50:42 +0100

    add website/signup_form.php

---
+++ website/signup_form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>signup_form</title>
+		<meta charset="utf-8">
+	</head>
+	<div align="center">
+		<h2>INSCRIPTION</h2>
+		<br /><br />
+		<form method="post" action="signup.php">
+			<label for="email">Pseudo :</label>
+			<input type="text" name="email" placeholder="Enter your email" autocomplete="off"/>
+			<br/>
+			<label for="password">Password :</label>
+			<input type="password" name="password" placeholder="Choose a password" autocomplete="off"/>
+			<br/><br/>
+			<input type="submit" name="signupform" value="SUBMIT">
+		</form>
+		<?php
+			if (isset($_GET['error']))
+			{
+				echo '<br/>' . $_GET['error'];
+				$_GET['error'] = NULL;
+			}
+		?>
+	</div>
+</html>
