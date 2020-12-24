b84dca9f59c33e4fcc3651f8683d1a569f99b3a0
nbousset <nbousset@student.42.fr>
2020-12-24 21:18:45 +0100

    add reset.sh

---
+++ reset.sh
@@ -0,0 +1,21 @@
+# reset iptables
+iptables -F
+rm /etc/iptables/rules.v4
+systemctl stop netfilter-persistent
+# reset ipset
+ipset flush
+ipset destroy
+rm /etc/iptables/sets.v4
+systemctl stop ipset-persistent
+rm /etc/systemd/system/ipset-persistent.service
+rm /etc/systemd/system/netfilter-persistent.service.requires/ipset-persistent.service
+rm /usr/local/sbin/ipset-restore.sh
+rm /usr/local/sbin/ipset-save.sh
+# reset crons
+rm /usr/local/sbin/update_script.sh
+rm /usr/local/sbin/watch_cron.sh
+crontab -u root -r
+# reset site
+rm -r /var/www/roger-skyline
+rm /etc/nginx/sites-available/roger-skyline
+rm /etc/nginx/sites-enabled/roger-skyline
